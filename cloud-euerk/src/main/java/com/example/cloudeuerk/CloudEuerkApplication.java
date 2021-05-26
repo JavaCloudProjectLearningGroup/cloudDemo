79b14622eafe354b74c51a7b08b9275cd5e159c8
xuxichen <xuxichen@cmcc>
2021-05-26 21:12:47 +0800

    从spring官网开始项目

---
+++ cloud-euerk/src/main/java/com/example/cloudeuerk/CloudEuerkApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudeuerk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEuerkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEuerkApplication.class, args);
+	}
+
+}
