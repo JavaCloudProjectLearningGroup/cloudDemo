e703426be01afc07fcdf330264077ec4a8cc9b4e
xuxichen <xuxichen@cmcc>
2021-06-03 11:26:00 +0800

    船舰初始目标

---
+++ cloud-euerka/src/test/java/com/example/cloudeuerk/CloudEuerkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudeuerk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEuerkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
